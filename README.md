bf130fca1dc829e2f7077a53caa0b6fb213b00e6
Andreas K. Huettel (dilfridge) <dilfridge@gentoo.org>
2014-01-18 23:18:34 +0100

    initial import of old files

--- README.md
+++
@@ -1,4 +0,0 @@
-gentooplotter
-=============
-
-scripts to plot various Gentoo-related measures over time
---
+++ bin/gentoo-tree-report.eval.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python2.7
+
+from collections import Counter
+import sys
+
+freq = Counter()
+total = 0
+
+for l in sys.stdin.readlines():
+    freq.update(l.strip().split())
+    total+=1
+
+print total,
+
+arches=sys.argv[1:]
+
+for arch in arches:
+    print freq['~'+arch]+freq[arch],
+    print freq[arch],
+
