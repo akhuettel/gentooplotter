e40473a916f3cc95d16ae75b9c8c6b6c82eb665a
Andreas K. Huettel (dilfridge) <dilfridge@gentoo.org>
2014-02-15 15:15:00 +0100

    bugfixes

--- html/eapi.php
+++ html/eapi.php
@@ -7,14 +7,17 @@ include("header.php");
 <a>
   <img src='eapi/month.png'>
 </a>
+<br>
 
 <a>
   <img src='eapi/year.png'>
 </a>
+<br>
 
 <a>
   <img src='eapi/all.png'>
 </a>
+<br>
 
 <?php
 include("header.php");
