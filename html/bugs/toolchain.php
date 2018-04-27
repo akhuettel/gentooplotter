3da14ecf74767f643b6fc3bb48518b45bf9a279b
Andreas K. Hüttel <dilfridge@gentoo.org>
2018-04-27 21:56:36 +0200

    Add toolchain bugs page

---
+++ html/bugs/toolchain.php
@@ -0,0 +1,16 @@
+<?php
+include("../private/header.php");
+?>
+
+<h1>Gentoo Toolchain bugs</h1>
+
+<a 
+href='https://bugs.gentoo.org/buglist.cgi?bug_status=UNCONFIRMED&bug_status=CONFIRMED&bug_status=IN_PROGRESS&email1=toolchain%40gentoo.org&emailassigned_to1=1&emailcc1=1&emailtype1=substring&list_id=120675&query_format=advanced&/' 
+target='_blank'>
+  <img src='plot-year-32.png'>
+</a>
+
+</body>
+</html>
+
+
