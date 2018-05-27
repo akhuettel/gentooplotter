ffe06958eb256a55b2643b71f47b599c6231704a
Andreas K. Hüttel <dilfridge@gentoo.org>
2018-05-27 21:56:21 +0200

    Add links to all currently active modules

--- html/index.php
+++ html/index.php
@@ -5,7 +5,10 @@ include("header.php");
 <h1>Gentoo graphs</h1>
 
 <ul>
-<li><a href="eapi.php">EAPIs in the main tree</a>
+<li><a href="eapi.php">EAPI distribution</a>
+<li><a href="keywords.php">Keywords and ~arch versus stable</a>
+<li><a href="googlecode.php">googlecode.com usage</a>
+<li><a href="fedorahosted.php">fedorahosted.org usage</a>
 </ul>
 
 <?php
