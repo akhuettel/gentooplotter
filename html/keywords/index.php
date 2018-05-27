0de2422ea19264b48ef5ee05f6f6efee771ae115
Andreas K. Hüttel <dilfridge@gentoo.org>
2018-05-27 19:23:27 +0200

    First attempt at the keyword number plots
    (cherry picked from commit ec707a6df9b1848cf9f645316cafe257f72b015b)

---
+++ html/keywords/index.php
@@ -0,0 +1,53 @@
+<?php
+include("../private/header.php");
+?>
+
+<h1>Gentoo ebuild numbers</h1>
+  <img src='all-major.png'><br>
+  <img src='all-prefix.png'><br>
+  <img src='all-freebsd.png'><br>
+  <img src='all-0.png'><br>
+  <img src='all-1.png'><br>
+  <img src='all-3.png'><br>
+  <img src='all-5.png'><br>
+  <img src='all-7.png'><br>
+  <img src='all-9.png'><br>
+  <img src='all-11.png'><br>
+  <img src='all-13.png'><br>
+  <img src='all-15.png'><br>
+  <img src='all-17.png'><br>
+  <img src='all-19.png'><br>
+  <img src='all-21.png'><br>
+  <img src='all-23.png'><br>
+  <img src='all-25.png'><br>
+  <img src='all-27.png'><br>
+  <img src='all-29.png'><br>
+  <img src='all-31.png'><br>
+  <img src='all-33.png'><br>
+  <img src='all-35.png'><br>
+  <img src='all-37.png'><br>
+  <img src='all-39.png'><br>
+  <img src='all-41.png'><br>
+  <img src='all-43.png'><br>
+  <img src='all-45.png'><br>
+  <img src='all-47.png'><br>
+  <img src='all-49.png'><br>
+  <img src='all-51.png'><br>
+  <img src='all-53.png'><br>
+  <img src='all-55.png'><br>
+  <img src='all-57.png'><br>
+  <img src='all-59.png'><br>
+  <img src='all-61.png'><br>
+  <img src='all-63.png'><br>
+  <img src='all-65.png'><br>
+  <img src='all-67.png'><br>
+  <img src='all-69.png'><br>
+  <img src='all-71.png'><br>
+  <img src='all-73.png'><br>
+  <img src='all-75.png'><br>
+  <img src='all-77.png'><br>
+  <img src='all-79.png'><br>
+
+<?php
+include("../private/footer.php");
+?>
