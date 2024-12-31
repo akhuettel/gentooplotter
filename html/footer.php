5f03bbb16cd788bdcb9a1c050ae282fc42b3639e
Andreas K. Hüttel <dilfridge@gentoo.org>
2024-12-31 12:03:30 +0100

    Update footer
    Signed-off-by: Andreas K. Hüttel <dilfridge@gentoo.org>

--- html/footer.php
+++ html/footer.php
@@ -1,15 +1,15 @@
 <p><br><br><br>
 
 <div id="footer">
-<CENTER><br>
-<A HREF="http://www.akhuettel.de/private/">personal page</A>
-- <A HREF="http://www.akhuettel.de/private//likes-dislikes.php">about me</A>
+<CENTER>
+<br>
+<A HREF="https://www.akhuettel.de/private/">personal page</A>
 - <A HREF="http://blog.akhuettel.de/">blog</A>
-- <A HREF="http://www.facebook.com/akhuettel">facebook</A>
-- <A HREF="http://www.akhuettel.de/Bilder/">pictures</A>
-- <A HREF="http://www.akhuettel.de/">work page</A>
-- <A HREF="http://www.akhuettel.de/private/pgp_key.php">PGP</A>
-- <A HREF="http://www.akhuettel.de/private/impressum.php">impressum</A>
+- <A HREF="https://instagram.com/akhuettel">instagram</A>
+- <A HREF="https://photos.akhuettel.de/">photos</A>
+- <A HREF="https://www.akhuettel.de/">work page</A>
+- <A HREF="https://www.akhuettel.de/private/pgp_key.php">openpgp</A>
+- <A HREF="https://www.akhuettel.de/private/impressum.php">impressum</A>
 </CENTER>
 </div>
 
