1bb057850a75ee620c9709271fa2437fa3e3cda7
Andreas K. Hüttel <dilfridge@gentoo.org>
2025-05-07 18:41:10 +0200

    Use css from main website
    Signed-off-by: Andreas K. Hüttel <dilfridge@gentoo.org>

--- html/header.php
+++ html/header.php
@@ -2,7 +2,7 @@
 <HEAD>
    <META HTTP-EQUIV="Content-Type" CONTENT="text/html; charset=iso-8859-1">
    <TITLE>Gentoo Graphs</TITLE>
-   <link rel=stylesheet type="text/css" href="/private/default.css">
+   <link rel=stylesheet type="text/css" href="https://www.akhuettel.de/private/default.css">
 </HEAD>
 
 <BODY>
